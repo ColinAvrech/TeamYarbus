499d882e388ffbfcf4127db819efe3d9b3d3affe
manaskulkarni <manas.kulkarni@digipen.edu>
2014-09-18 01:45:22 -0700

    Update BuildSystem.bat

--- Engine/CinderEngine/tools/BuildSystem.bat
+++ Engine/CinderEngine/tools/BuildSystem.bat
@@ -2,7 +2,7 @@
 set /p systemName=System Name:
 
 md ..\source\Engine\System\systemName
-copy /y template\SystemTemplate.cpp ..\source\Objects_Game\%systemName%.cpp
-copy /y template\SystemTemplate.h ..\include\Objects_Game\%systemName%.h
+copy /y template\SystemTemplate.cpp ..\source\Engine\System\%systemName%.cpp
+copy /y template\SystemTemplate.h ..\include\Engine\System\%systemName%.h
 
 pause
