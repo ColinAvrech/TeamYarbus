f2ecc2f705c775296573cd4a05cd0beac5647503
Sagnik Chowdhury <sagnik.c@digipen.edu>
2014-09-18 22:28:38 -0700

    Most of the simpler functions for the thermodynamics system are in. Loading the grid depends on design choices that have not been yet apparently (mostly involving world transform unit system). The Zilch component is partially done but I'll need serialization to test it so I'll start work on that first.

---
+++ Engine/CinderEngine/include/Engine/System/Zilch/ScriptComponent.h
@@ -0,0 +1,70 @@
+/*****************************************************************************/
+/*!
+\file   ScriptComponent.h
+\author Sagnik Chowdhury
+\par    Course: GAM200
+\par    All content 2014 DigiPen (USA) Corporation, all rights reserved.
+\brief  Scriptcomponents bind components from Zilch to the engine.
+*/
+/*****************************************************************************/
+
+//!makes sure this is only included once
+#pragma once
+
+#include "Common.h"
+#include "Component.h"
+#include "Zilch.hpp"
+
+namespace Framework
+{
+  namespace ScriptSystem
+  {
+    //NOTES-------------------------------------------------------------
+    //To later check if a GameComponent is a ZilchComponent:
+    //GameComponent *c = ....
+    //ZilchComponent *zc = dynamic_cast<ZilchComponent*>(c);
+    //if(zc != 0) // is ZilchComponent
+
+    //pass std::string str.c_str() to convert to const char*
+    //------------------------------------------------------------------
+
+    class ZilchComponent : public GameComponent
+    {
+    public:
+
+      //Constructor. Initialize with owner and component name.
+      ZilchComponent(GameObject *obj, std::string* name);
+
+      //!Updated every frame.
+      void Update(const double dt);
+
+      //!Initialize the system.
+      virtual bool Initialize();
+
+      /*!TODO IMPLIMENT SERIALIZATION!*/
+      virtual void Serialize(){};
+
+      //Destructor
+      ~ZilchComponent();
+
+#ifdef _DEBUG
+    private:
+      //Private data ----------------------------------
+      //Zilch class name
+      Zilch::StringParam ZilchClass;
+      //Zilch class (Zilch::BoundType*)
+      Zilch::BoundType* This;
+      //Zilch library for this component
+      Zilch::LibraryRef* library;
+      //Zilch functions (Zilch::Function*)
+      //Initialize
+      Zilch::Function* ZilchInitialize;
+      //Update
+      Zilch::Function* Run;
+      //Zilch exception handler (Zilch::ExceptionReport)
+      Zilch::ExceptionReport report;
+
+#endif
+    }; //class scriptcomponent
+  }  //namespace ScriptSystem
+}  //namespace framework
---
+++ Engine/CinderEngine/source/Engine/System/Zilch/ZilchComponent.cpp
@@ -0,0 +1,45 @@
+#include "ScriptComponent.h"
+#include "ZilchCompiledLib.h"
+
+namespace Framework
+{
+  namespace ScriptSystem
+  {
+    //CONSTRUCTOR
+    ZilchComponent::ZilchComponent(GameObject *obj, std::string* name)
+      : GameComponent(obj), ZilchClass(Zero::String(name->c_str()))
+    {
+      //Do Stuff
+    }
+
+    //INITIALIZE
+    bool ZilchComponent::Initialize()
+    {
+      //Get pointer to appropriate library
+      //library = SCRIPTSYSTEM->GetZilchLib(ZilchClass.c_str());
+      //Bind the Zilch class
+      This = (*library)->BoundTypes.findValue(ZilchClass, nullptr);
+      ErrorIf(This == nullptr, "Failed to find a Zilch type named ", ZilchClass);
+
+      //Create an array of arguments
+      Zilch::Array<Zilch::Type*> args;
+
+      //Find the Initialize function
+      // We pass in an array of types to specify the arguments we want, in this case its an empty array
+      // We also pass in the void type because we don't expect a return value
+      ZilchInitialize = This->FindFunction("Initialize", args, ZilchTypeId(void), Zilch::FindMemberOptions::None);
+      ErrorIf(ZilchInitialize == nullptr, "Failed to find function 'Initialize' on Zilch type ", ZilchClass);
+
+      // Setup the console so that when we call 'Console.WriteLine' it outputs to stdio
+      Zilch::Console::AddWriteTextCallback(Zilch::DefaultWriteText, nullptr);
+
+      // We can also setup the console so that any 'Read' functions will attempt to read from stdin
+      Zilch::Console::SetReadTextCallback(Zilch::DefaultReadText, nullptr);
+
+      
+
+      return true;
+    }
+   
+  }
+}
