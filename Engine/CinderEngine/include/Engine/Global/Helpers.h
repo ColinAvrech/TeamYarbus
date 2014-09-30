ad2fb36287caeb0748ec2b757ed53c0eb540f3d6
MicahRust <micahtim@gmail.com>
2014-09-30 03:54:50 -0700

    MoreFiles!

---
+++ Engine/CinderEngine/include/Engine/Global/Helpers.h
@@ -0,0 +1,14 @@
+/******************************************************************************/
+/*!
+\file   Helpers.h
+\author Micah Rust
+\par    Course: GAM200
+\par    All content 2014 DigiPen (USA) Corporation, all rights reserved.
+\brief  This file contains several helper macros for programming
+*/
+/******************************************************************************/
+
+#pragma once
+
+#define FOREVER() for(true)
+#define RETURNIF(exp) do { if ((exp)) return;}  while (0)
