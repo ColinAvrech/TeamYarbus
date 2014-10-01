cc8a5422bb7d0ee4e850ea7c017432677c5cc4dc
Sagnik Chowdhury <sagnik.c@digipen.edu>
2014-09-30 22:43:13 -0700

    Fixed build errors

--- Engine/CinderEngine/include/Engine/System/Physics/Resolution.h
+++ Engine/CinderEngine/include/Engine/System/Physics/Resolution.h
@@ -3,14 +3,14 @@
 #pragma once
 
 #include "RigidBody.h"
-#include ""
+#include "CollisionEvent.h"
 namespace Framework
 {
 	namespace Physics
 	{
 
-		void Resolve(Manifold* pre);
-		void ResolveStatic(Manifold* pre, Manifold* post);
-		void ResolveDynamic(Manifold* pre, Manifold* post);
+		//void Resolve(Manifold* pre);
+		//void ResolveStatic(Manifold* pre, Manifold* post);
+		//void ResolveDynamic(Manifold* pre, Manifold* post);
 	}
 }
