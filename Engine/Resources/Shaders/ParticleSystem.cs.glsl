138747446ef34c13cb979911beb6d0e91129e36e
THEAK472009 <manas.kulkarni@digipen.edu>
2014-10-24 15:04:31 -0700

    Basic GPU Particle System Implementation

--- Engine/Resources/Shaders/ParticleSystem.cs.glsl
+++ Engine/Resources/Shaders/ParticleSystem.cs.glsl
@@ -1,37 +1,23 @@
 #version 430
 
-struct ParticleData
-{
-    vec4 m_pos;
-    vec4 m_col;
-    vec4 m_startCol;
-    vec4 m_endCol;
-    vec4 m_vel;
-    vec4 m_acc;
-    vec4 m_time;
-    bool  m_alive;
-    uint m_count;
-    uint m_countAlive;
-};
-
 // Target 0 : Vertex position
-layout(std140, binding = 0) buffer Pos
+layout(std140, binding = 0) buffer Col
 {
-   vec4 Positions [ ];
+   vec4 Colors [ ];
 };
 
 layout (local_size_x = 16, local_size_y = 16) in;
 
-vec4 vel = vec4 (0, 1, 0, 0);
-uniform float deltaT;
+uniform int particleCount;
 
 void main ()
 {
   // Current SSBO index
   uint index = gl_GlobalInvocationID.x;
-  vec4 pos = Positions [index];
-
-  pos += deltaT * vel;
-
-  Positions [index] = pos;
+  if (index < particleCount)
+  {
+    vec4 col = Colors [index];
+    col = vec4 (1, 0, 0, 1);
+    Colors [index] = col;
+  }
 }
