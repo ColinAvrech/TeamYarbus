8ae70a5a766b7844c891dac34a32419375f7124d
Filiecs <Filiecs2@gmail.com>
2014-10-27 21:01:15 -0700

    Just changed the level to physics level.

--- Engine/CinderEngine/source/Engine/WinMain.cpp
+++ Engine/CinderEngine/source/Engine/WinMain.cpp
@@ -137,7 +137,7 @@ int main (void)
   //audio->LoadMicData ();
 
   //! activate the window.
-  //OBJECTSYSTEM->LoadLevel ("PhysicsTest.data");
+  OBJECTSYSTEM->LoadLevel ("PhysicsTest.data");
 
   // Connect example
   //MyClass _myclass;
