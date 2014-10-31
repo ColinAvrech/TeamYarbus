e750b8043b26b61b517e0832ec6bb436811e91b2
THEAK472009 <manas.kulkarni@digipen.edu>
2014-10-30 18:09:02 -0700

    --

--- Engine/CinderEngine/source/Engine/System/WindowSystems/Graphics/ParticleSystem/CLParticleRenderer.cpp
+++ Engine/CinderEngine/source/Engine/System/WindowSystems/Graphics/ParticleSystem/CLParticleRenderer.cpp
@@ -23,72 +23,72 @@ namespace Framework
   static float breathRate = 0.01f;
   static float offset = 0.75f;
 
-  static float random(float fMin, float fMax)
+  static float random (float fMin, float fMax)
   {
-    float fRandNum = (float)rand() / RAND_MAX;
+    float fRandNum = (float) rand () / RAND_MAX;
     return fMin + (fMax - fMin) * fRandNum;
   }
 
 
-  CLParticleRenderer::CLParticleRenderer()
+  CLParticleRenderer::CLParticleRenderer ()
   {
     particleCount = 10000;
     particleSize = 5;
-    srand((unsigned)time(NULL));
-    color[0] = 255;
-    color[1] = 80;
-    color[2] = 0;
-    color[3] = 0.1f;
+    srand ((unsigned) time (NULL));
+    color [0] = 255;
+    color [1] = 80;
+    color [2] = 0;
+    color [3] = 0.1f;
     colorChangeTimer = 1000.0f;
   }
 
-  CLParticleRenderer::~CLParticleRenderer()
+  CLParticleRenderer::~CLParticleRenderer ()
   {
     delete SSBOPos, SSBOVel, vao;
   }
 
-  void CLParticleRenderer::GenerateShaders()
+  void CLParticleRenderer::GenerateShaders ()
   {
-    shader = Resources::RS->Get_Shader("Simple");
-    computeshader = Resources::RS->Get_ComputeShader("Velocities.cl.glsl");
+    shader = Resources::RS->Get_Shader ("Simple");
+    computeshader = Resources::RS->Get_ComputeShader ("Velocities.cl.glsl");
   }
 
 
-  void CLParticleRenderer::GenerateBuffers()
+  void CLParticleRenderer::GenerateBuffers ()
   {
     // OpenGL 3.3+
     // Create a VAO and never use it again!!!
-    vao = new VAO();
-    SSBOPos = new SSBO(particleCount * sizeof(glm::vec4));
+    vao = new VAO ();
+    SSBOPos = new SSBO (particleCount * sizeof(glm::vec4));
 
     // Fill
-    ResetPosition();
+    ResetPosition ();
     // Bind buffer to target index 0
-    SSBOPos->BindBufferBase(0);
+    SSBOPos->BindBufferBase (0);
 
 
-    SSBOVel = new SSBO(particleCount * sizeof(glm::vec4));
-    ResetVelocity();
+    SSBOVel = new SSBO (particleCount * sizeof(glm::vec4));
+    ResetVelocity ();
     // Bind buffer to target index 1
-    SSBOVel->BindBufferBase(1);
+    SSBOVel->BindBufferBase (1);
   }
 
-  void CLParticleRenderer::ResetBuffers()
+  void CLParticleRenderer::ResetBuffers ()
   {
-    SSBOPos->BindBuffer();
-    ResetPosition();
-    SSBOVel->BindBuffer();
-    ResetVelocity();
+    SSBOPos->BindBuffer ();
+    ResetPosition ();
+    SSBOVel->BindBuffer ();
+    ResetVelocity ();
   }
 
 
-  void CLParticleRenderer::OnKeyPressed(KeyEvent* key)
+  void CLParticleRenderer::OnKeyPressed (KeyEvent* key)
   {
     std::cout << cursorX << std::endl;
     if (key->KeyDown)
     {
       if (key->KeyValue == GLFW_KEY_R)
-        ResetBuffers();
+        ResetBuffers ();
       if (key->KeyValue == GLFW_KEY_A)
         speedMultiplier -= 0.1f;
       else if (key->KeyValue == GLFW_KEY_D)
@@ -104,96 +104,96 @@ namespace Framework
   }
 
 
-  void CLParticleRenderer::GenerateTextures()
+  void CLParticleRenderer::GenerateTextures ()
   {
-    texture = Resources::RS->Get_Texture("Particle.bmp");
-    cursorX = WINDOWSYSTEM->Get_Width() / 2;
-    cursorY = WINDOWSYSTEM->Get_Height() / 2;
+    texture = Resources::RS->Get_Texture ("Particle.bmp");
+    cursorX = WINDOWSYSTEM->Get_Width () / 2;
+    cursorY = WINDOWSYSTEM->Get_Height () / 2;
 
-    EVENTSYSTEM->mConnect<KeyEvent, CLParticleRenderer>(Events::KEY_ANY, this, &CLParticleRenderer::OnKeyPressed);
+    EVENTSYSTEM->mConnect<KeyEvent, CLParticleRenderer> (Events::KEY_ANY, this, &CLParticleRenderer::OnKeyPressed);
   }
 
-  void CLParticleRenderer::ResetPosition()
+  void CLParticleRenderer::ResetPosition ()
   {
     // Reset to mouse cursor pos
     //double cursorX, cursorY;
     int windowWidth, windowHeight;
-    glfwPollEvents();
-    glfwGetCursorPos(WINDOWSYSTEM->Get_Window(), &cursorX, &cursorY);
-    glfwGetWindowSize(WINDOWSYSTEM->Get_Window(), &windowWidth, &windowHeight);
+    glfwPollEvents ();
+    glfwGetCursorPos (WINDOWSYSTEM->Get_Window (), &cursorX, &cursorY);
+    glfwGetWindowSize (WINDOWSYSTEM->Get_Window (), &windowWidth, &windowHeight);
 
-    destPosX = (float)(cursorX / (windowWidth)-0.5f) * 2.0f;
-    destPosY = (float)((windowHeight - cursorY) / windowHeight - 0.5f) * 2.0f;
+    destPosX = (float) (cursorX / (windowWidth) -0.5f) * 2.0f;
+    destPosY = (float) ((windowHeight - cursorY) / windowHeight - 0.5f) * 2.0f;
 
-    glm::vec4* verticesPos = (glm::vec4*) SSBOPos->MapBufferRange<glm::vec4>(0, particleCount);
+    glm::vec4* verticesPos = (glm::vec4*) SSBOPos->MapBufferRange<glm::vec4> (0, particleCount);
     for (int i = 0; i < particleCount; i++)
     {
-      float rnd = (float)rand() / (float)(RAND_MAX);
-      float rndVal = (float)rand() / (float)(RAND_MAX / (360.0f * 3.14f * 2.0f));
-      float rndRad = (float)rand() / (float)(RAND_MAX)* 0.05f;
+      float rnd = (float) rand () / (float) (RAND_MAX);
+      float rndVal = (float) rand () / (float) (RAND_MAX / (360.0f * 3.14f * 2.0f));
+      float rndRad = (float) rand () / (float) (RAND_MAX) * 0.05f;
       radius = rndRad;
-      verticesPos[i].x = destPosX + cos(rndVal) * rndRad;
-      verticesPos[i].y = destPosY + sin(rndVal) * rndRad;
-      verticesPos[i].z = 0.0f;
-      verticesPos[i].w = 1.0f;
+      verticesPos [i].x = destPosX + cos (rndVal) * rndRad;
+      verticesPos [i].y = destPosY + sin (rndVal) * rndRad;
+      verticesPos [i].z = 0.0f;
+      verticesPos [i].w = 1.0f;
       //Physics::THERMODYNAMICS->SetCellTemperature (verticesPos [i].x * 2, verticesPos [i].y * 2, 2250.0f, 0.016);
     }
-    SSBOPos->UnMapBuffer();
+    SSBOPos->UnMapBuffer ();
   }
 
 
-  void CLParticleRenderer::ResetVelocity()
+  void CLParticleRenderer::ResetVelocity ()
   {
-    glm::vec4* verticesVel = SSBOVel->MapBufferRange<glm::vec4>(0, particleCount);
+    glm::vec4* verticesVel = SSBOVel->MapBufferRange<glm::vec4> (0, particleCount);
     for (int i = 0; i < particleCount; i++)
     {
-      verticesVel[i].x = 0.0f;
-      verticesVel[i].y = 0.0f;
-      verticesVel[i].z = 0.0f;
-      verticesVel[i].w = 1.0f;
+      verticesVel [i].x = 0.0f;
+      verticesVel [i].y = 0.0f;
+      verticesVel [i].z = 0.0f;
+      verticesVel [i].w = 1.0f;
     }
-    SSBOVel->UnMapBuffer();
+    SSBOVel->UnMapBuffer ();
   }
 
 
-  void CLParticleRenderer::Render()
+  void CLParticleRenderer::Render ()
   {
-    Interpolate_Colors();
-    vao->bindVAO();
-    SSBOPos->BindBufferBase(0);
-    SSBOVel->BindBufferBase(1);
+    Interpolate_Colors ();
+    vao->bindVAO ();
+    SSBOPos->BindBufferBase (0);
+    SSBOVel->BindBufferBase (1);
 
-    if (AUDIOSYSTEM->input.peaklevel[0] > 0.05f)
+    if (AUDIOSYSTEM->input.peaklevel [0] > 0.05f)
     {
-      if (color[3] < 1) color[3] += AUDIOSYSTEM->input.peaklevel[0] * 0.1f;
+      if (color [3] < 1) color [3] += AUDIOSYSTEM->input.peaklevel [0] * 0.1f;
       //printf("%f\n", color[3]);
     }
     else
     {
-      if (color[3] > 0.1f) color[3] -= 0.016f;
+      if (color [3] > 0.1f) color [3] -= 0.016f;
     }
-    double frameTimeStart = glfwGetTime() * 1000;
+    double frameTimeStart = glfwGetTime () * 1000;
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+    glEnable (GL_BLEND);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE);
 
     // Run compute shader
 
     //double cursorX, cursorY;
     int windowWidth, windowHeight;
-    glfwGetCursorPos(WINDOWSYSTEM->Get_Window(), &cursorX, &cursorY);
-    glfwGetWindowSize(WINDOWSYSTEM->Get_Window(), &windowWidth, &windowHeight);
+    glfwGetCursorPos (WINDOWSYSTEM->Get_Window (), &cursorX, &cursorY);
+    glfwGetWindowSize (WINDOWSYSTEM->Get_Window (), &windowWidth, &windowHeight);
 
-    destPosX = (float)(cursorX / (windowWidth)-0.5f) * 2.0f;
-    destPosY = (float)((windowHeight - cursorY) / windowHeight - 0.5f) * 2.0f;
+    destPosX = (float) (cursorX / (windowWidth) -0.5f) * 2.0f;
+    destPosY = (float) ((windowHeight - cursorY) / windowHeight - 0.5f) * 2.0f;
 
-    computeshader->Use();
-    computeshader->uni1f("deltaT", 2 * speedMultiplier * (pause ? 0 : 1));
-    computeshader->uni3f("destPos", destPosX, destPosY, 0);
-    computeshader->uni2f("vpDim", 1, 1);
-    computeshader->uni1i("borderClamp", (int)borderEnabled);
+    computeshader->Use ();
+    computeshader->uni1f ("deltaT", 2 * speedMultiplier * (pause ? 0 : 1));
+    computeshader->uni3f ("destPos", destPosX, destPosY, 0);
+    computeshader->uni2f ("vpDim", 1, 1);
+    computeshader->uni1i ("borderClamp", (int) borderEnabled);
     //std::cout << "{ " << Physics::THERMODYNAMICS->GetCellVelocity(20, 20).x << ", " << Physics::THERMODYNAMICS->GetCellVelocity(20, 20).y << " }\n";
-    computeshader->uni2fv("cellVelocity", glm::value_ptr(Physics::THERMODYNAMICS->GetCellVelocity(20, 20)));
+    computeshader->uni2fv ("cellVelocity", glm::value_ptr (Physics::THERMODYNAMICS->GetCellVelocity (20, 20)));
     radius = 0.1f;
     float dy = -destPosY;
     //std::cout << Physics::THERMODYNAMICS->GetCellTemperature (20, 20) << "\n";
@@ -207,37 +207,37 @@ namespace Framework
 
     int workingGroups = particleCount / 16;
 
-    computeshader->Dispatch_Compute(workingGroups + 1, 1, 1);
+    computeshader->Dispatch_Compute (workingGroups + 1, 1, 1);
 
-    computeshader->Disable();
+    computeshader->Disable ();
 
     // Set memory barrier on per vertex base to make sure we get what was written by the compute shaders
-    glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
+    glMemoryBarrier (GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
 
     // Render scene
 
-    shader->Use();
+    shader->Use ();
 
-    shader->uni4f("Color", color[0] / 255.0f, color[1] / 255.0f, color[2] / 255.0f, 1.0f);
+    shader->uni4f ("Color", color [0] / 255.0f, color [1] / 255.0f, color [2] / 255.0f, 1.0f);
 
-    glGetError();
+    glGetError ();
 
-    texture->Bind();
-    GLuint posAttrib = shader->attribLocation("position");
+    texture->Bind ();
+    GLuint posAttrib = shader->attribLocation ("position");
 
-    glBindBuffer(GL_ARRAY_BUFFER, SSBOPos->Get_POS());
-    shader->vertexAttribPtr(posAttrib, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    shader->enableVertexAttribArray(posAttrib);
-    glPointSize(particleSize);
-    glDrawArrays(GL_POINTS, 0, particleCount);
+    glBindBuffer (GL_ARRAY_BUFFER, SSBOPos->Get_POS ());
+    shader->vertexAttribPtr (posAttrib, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    shader->enableVertexAttribArray (posAttrib);
+    glPointSize (particleSize);
+    glDrawArrays (GL_POINTS, 0, particleCount);
 
-    texture->Unbind();
-    shader->Disable();
+    texture->Unbind ();
+    shader->Disable ();
 
-    frameDelta = (float)(glfwGetTime() - frameTimeStart) * 1000.0f;
+    frameDelta = (float) (glfwGetTime () - frameTimeStart) * 1000.0f;
   }
 
-  void CLParticleRenderer::Interpolate_Colors()
+  void CLParticleRenderer::Interpolate_Colors ()
   {
     if (colorFade)
     {
@@ -245,35 +245,35 @@ namespace Framework
 
       if ((colorChangeTimer <= 0.0f) & (colorChangeLength <= 0.0f))
       {
-        if (random(0.0f, 100.0f) < 50.0f)
+        if (random (0.0f, 100.0f) < 50.0f)
         {
-          colVec[0] = (int)random(0.0f, 8.0f) * 32.0f - color[0];
-          colVec[1] = (int)random(0.0f, 8.0f) * 32.0f - color[1];
-          colVec[2] = (int)random(0.0f, 8.0f) * 32.0f - color[2];
+          colVec [0] = (int) random (0.0f, 8.0f) * 32.0f - color [0];
+          colVec [1] = (int) random (0.0f, 8.0f) * 32.0f - color [1];
+          colVec [2] = (int) random (0.0f, 8.0f) * 32.0f - color [2];
 
-          float vlength = sqrt(colVec[0] * colVec[0] + colVec[1] * colVec[1] + colVec[2] * colVec[2]);
+          float vlength = sqrt (colVec [0] * colVec [0] + colVec [1] * colVec [1] + colVec [2] * colVec [2]);
           colorChangeLength = vlength * 2.0f;
 
-          colVec[0] = colVec[0] / vlength;
-          colVec[1] = colVec[2] / vlength;
-          colVec[2] = colVec[2] / vlength;
+          colVec [0] = colVec [0] / vlength;
+          colVec [1] = colVec [2] / vlength;
+          colVec [2] = colVec [2] / vlength;
         }
         colorChangeTimer = 1000.0f;
       }
 
       if (colorChangeLength > 0.0f)
       {
-        color[0] += colVec[0] * frameDelta;
-        color[1] += colVec[1] * frameDelta;
-        color[2] += colVec[2] * frameDelta;
+        color [0] += colVec [0] * frameDelta;
+        color [1] += colVec [1] * frameDelta;
+        color [2] += colVec [2] * frameDelta;
         colorChangeLength -= frameDelta;
       }
     }
     else
     {
-      color[0] = 255.0f;
-      color[1] = 64.0f;
-      color[2] = 0.0f;
+      color [0] = 255.0f;
+      color [1] = 64.0f;
+      color [2] = 0.0f;
     }
   }
 
