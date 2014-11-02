4b3b382c245bc2b0c35fa765a409fc24e44afa1e
Sagnik Chowdhury <sagnik.c@digipen.edu>
2014-11-02 10:46:35 -0800

    Added a fractal noise generator. Currently commented out as it causes heap corruption.

--- Engine/CinderEngine/include/Engine/Procedural_Content_Generation/FractalNoise.h
+++ Engine/CinderEngine/include/Engine/Procedural_Content_Generation/FractalNoise.h
@@ -1,6 +1,6 @@
 /*****************************************************************************/
 /*!
-\file   PerlinNoise.h
+\file   FractalNoise.h
 \author Sagnik Chowdhury
 \par    Course: GAM200
 \par    All content 2014 DigiPen (USA) Corporation, all rights reserved.
@@ -13,21 +13,25 @@ namespace Framework
   namespace Procedural
   {
     //2D perlin noise generator (1D array)
-    class PerlinNoise2D
+    class FractalNoise2D
     {
     public:
-      PerlinNoise2D(int size, int wavelength);
+      FractalNoise2D(int size, int passes);
+      ~FractalNoise2D();
     private:
       int *Array;
     };
 
     //3D perlin noise generator (2D array)
-    class PerlinNoise3D
+    class FractalNoise3D
     {
     public:
-      PerlinNoise3D(int width, int height, int wavelength);
+      FractalNoise3D(int width, int height, int passes);
+      ~FractalNoise3D();
     private:
-      int **Array;
+      float **Array;
+      int sizeX;
+      int sizeY;
     };
   } //procedural
 } //framework
---
+++ Engine/CinderEngine/source/Engine/Procedural_Content_Generation/FractalNoise.cpp
@@ -0,0 +1,88 @@
+/*****************************************************************************/
+/*!
+\file   FractalNoise.cpp
+\author Sagnik Chowdhury
+\par    Course: GAM200
+\par    All content 2014 DigiPen (USA) Corporation, all rights reserved.
+\brief  Generate a simplex fractal noise texture
+*/
+/*****************************************************************************/
+#include "FractalNoise.h"
+#include "glm/gtc/random.hpp"
+#include "glm/gtc/noise.hpp"
+#include <stdlib.h>
+#include <iostream>
+namespace Framework
+{
+  namespace Procedural
+  {
+    FractalNoise3D::FractalNoise3D(int width, int height, int passes) :
+      sizeX(width), sizeY(height)
+    {
+      //Array = new float*[width];
+      //for (int i = 0; i < sizeX; ++i)
+      //  Array[i] = new float[sizeY];
+
+      //float **Buffer = new float*[width];
+      //for (int i = 0; i < sizeX; ++i)
+      //  Buffer[i] = new float[sizeY];
+
+      //for (int i = 0; i < sizeY; i++)
+      //{
+      //  for (int j = 0; j < sizeX; j++)
+      //  {
+      //    int r = rand() % 2;
+      //    Buffer[i][j] = std::abs(glm::simplex(glm::vec2(r, r)));
+      //  }
+      //}
+      //for (int i = 0; i < sizeY; i++)
+      //{
+      //  for (int j = 0; j < sizeX; j++)
+      //  {
+      //    int r = rand() % 2;
+      //    Array[i][j] = 0;
+      //  }
+      //}
+      //int div = 1;
+      //for (int i = 0; i < passes; ++i)
+      //  div *= 2;
+      //float weight = 0.5f / div;
+      //int denom = 1;
+      //for (int k = 1; k < passes; ++k, weight *= 2, denom *= 2)
+      //{
+      //  for (int j = 0; j < sizeY; ++j)
+      //  {
+      //    for (int i = 0; i < sizeX; ++i)
+      //    {
+      //      Array[j][i] += weight * Buffer[j / denom][i / denom];
+      //    }
+      //  }
+      //}
+      ////for (int i = 0; i < sizeY; i++)
+      ////{
+      ////  for (int j = 0; j < sizeX; j++)
+      ////  {
+      ////    Array[i][j] -= 1;
+      ////  }
+      ////}
+
+      //for (int i = 0; i < sizeY; i++)
+      //{
+      //  for (int j = 0; j < sizeX; j++)
+      //  {
+      //    std::cout << Array[i][j] << " ";
+      //  }
+      //  std::cout << std::endl;
+      //}
+
+      //for (int i = 0; i < sizeX; ++i)
+      //  delete[] Buffer[i];
+      //delete[] Buffer;
+    }
+
+    FractalNoise3D::~FractalNoise3D()
+    {
+
+    }
+  } //procedural
+} //framework
