7d9be58684881e588f2bfd4d5c9dd2cce89197c8
Deepak Chennakkadan <deepak.chennakkadan@digipen.edu>
2014-11-16 21:38:13 -0800

    Fixed broken build. Re-check with others when merging. Don't have git hub open while merging. Adds weird things.

--- Engine/CinderEngine/source/Engine/Serializer/Serializer.cpp
+++ Engine/CinderEngine/source/Engine/Serializer/Serializer.cpp
@@ -46,12 +46,10 @@ namespace Framework
       {
         dataFile.close();
       }
-<<<<<<< HEAD
+
       string path("..//..//Resources//Levels//");
-=======
-      std::string path("..//..//Resources//Levels//");
 	  std::string extension(".data");
->>>>>>> b9c6c16c810141383b520f40cbed9461feed7743
+
       path.append(filepath);
 	  path.append(extension);
       dataFile.open(path);
@@ -59,17 +57,15 @@ namespace Framework
       ErrorIf(dataFile.is_open() == false, "Could not open Level File");
 
       //Skip first 2 lines
-<<<<<<< HEAD
+
       string dummy;
       std::getline(dataFile, dummy);
       std::getline(dataFile, dummy);
-=======
-      std::string dummy;
+
 	  std::string dummy2;
 	  dataFile >> dummy;
 	  std::getline(dataFile, dummy2);
 	  std::getline(dataFile, dummy2);
->>>>>>> b9c6c16c810141383b520f40cbed9461feed7743
       ++inObject;
       trunk = AddNode(trunk, TYPE_OBJECT, dummy.c_str(), 0);
       CurrentNode = CurrentStem = trunk;
