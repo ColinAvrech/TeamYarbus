aee4ee6e8763652f177fa1d9c8d1a6eca991a146
annapearson <anna.p@digipen.edu>
2014-11-26 17:26:50 -0800

    health.h & .cpp

---
+++ Engine/CinderEngine/include/Engine/Gameplay/Health.h
@@ -0,0 +1,39 @@
+/******************************************************************************/
+/*!
+\file   Health.h
+\author Anna Pearson
+\par    Course: GAM200
+\par    All content 2014 DigiPen (USA) Corporation, all rights reserved.
+\brief  The players health
+*/
+/******************************************************************************/
+#pragma once
+#include "Component.h"
+
+namespace Framework
+{
+	class UpdateEvent;
+	class Health : public Component
+	{
+	public:
+		const static std::string Name;
+		Health(GameObject * obj)
+		{
+			gameObject = obj;
+		}
+		Health(){}
+
+		~Health(){}
+		void reFuel();
+		void Update(UpdateEvent* e);
+		virtual void Serialize(Serializer::DataNode* data);
+		virtual void Initialize();
+
+	private:
+		float maxRadius;
+		float minRadius;
+		float currentRadius;
+		bool alive;
+		bool reFueling;
+	};
+}
