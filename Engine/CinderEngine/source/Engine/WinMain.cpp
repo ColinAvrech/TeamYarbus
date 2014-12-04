abf58a383f11d6e1650faf4b332cc82dd8464f80
Manquia <micahtim@gmail.com>
2014-12-03 20:25:25 -0800

    Fixed Small issue with ObjectSystem loading the level.

--- Engine/CinderEngine/source/Engine/WinMain.cpp
+++ Engine/CinderEngine/source/Engine/WinMain.cpp
@@ -137,6 +137,8 @@ int main (void)
   //EVENTSYSTEM->gDisconnect<UpdateEvent>(Framework::Events::UPDATEEVENT, &TestUEDisconnect);
 
 	//ZInterface::OpenFile();
+  // Update the ObjectManager to load in the first level
+  OBJECTSYSTEM->Update(0.016);
 
   //! Run the game! NOW!
   engine->GameLoop ();
