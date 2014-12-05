e264d3c9809d7845b17a18f561093ac822efa26a
THEAK472009 <manas.kulkarni@digipen.edu>
2014-12-04 16:14:50 -0800

    --

--- Engine/CinderEngine/source/Engine/System/ObjectSystem/ObjectSystem.cpp
+++ Engine/CinderEngine/source/Engine/System/ObjectSystem/ObjectSystem.cpp
@@ -56,6 +56,8 @@ deleted.
 #include "LevelTimer.h"
 #include "CheatCodes.h"
 #include "Follow.h"
+#include "InterpolateBackground.h"
+#include "SpriteColorUpdate.h"
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
@@ -204,6 +206,8 @@ namespace Framework
     RegisterComponent (FireStarter);
     RegisterComponent (LevelTimer);
     RegisterComponent (Follow);
+    RegisterComponent (InterpolateBackground);
+    RegisterComponent (SpriteColorUpdate);
     //////////////////////////////////////////////////////////////////////////
   }
 	void ObjectSystem::AddComponentCreator(string name, ComponentCreator* creator)
